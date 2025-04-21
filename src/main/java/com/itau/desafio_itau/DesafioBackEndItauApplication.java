f418659b8e6c50dedf0b4f3e749e99bb2d968812
edu_mb <eduardowemb@gmail.com>
2025-04-21 10:44:53 -0300

    projeto Spring Boot inicializado

---
+++ src/main/java/com/itau/desafio_itau/DesafioBackEndItauApplication.java
@@ -0,0 +1,13 @@
+package com.itau.desafio_itau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackEndItauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackEndItauApplication.class, args);
+	}
+
+}
